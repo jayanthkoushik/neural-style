91eeef623fcd11ad5b71b6faa2aededa0caf161b
Jayanth Koushik <jkoushik@cs.cmu.edu>
2017-01-04 10:10:51 -0500

    Replace transposed convolutions with upsampling, and use reflect padding

--- neural_style/fast_neural_style/transformer_net.py
+++ neural_style/fast_neural_style/transformer_net.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 import theano.tensor as T
-from keras.layers import Input, Conv2D, Activation, Lambda, merge
+from keras.layers import Input, Conv2D, Activation, Lambda, UpSampling2D, merge
 from keras.models import Model
 from keras.engine.topology import Layer
 
@@ -28,64 +28,66 @@ class InstanceNormalization(Layer):
         return self.scale.dimshuffle("x", 0, "x", "x") * y + self.shift.dimshuffle("x", 0, "x", "x")
 
 
-class TransConv2D(Layer):
+class ReflectPadding2D(Layer):
 
-    def __init__(self, nb_filter, filter_length, subsample, **kwargs):
-        self.nb_filter = nb_filter
-        self.filter_length = filter_length
-        self.subsample = subsample
+    def __init__(self, padding=(1, 1), **kwargs):
+        self.padding = padding
         super().__init__(**kwargs)
 
     def build(self, input_shape):
-        self.filters = self.add_weight(shape=(input_shape[1], self.nb_filter, self.filter_length, self.filter_length), initializer="glorot_uniform", trainable=True)
-        self.bias = self.add_weight(shape=(self.nb_filter,), initializer="zero", trainable=True)
         super().build(input_shape)
 
     def call(self, x, mask=None):
-        output_shape = (None, self.nb_filter, x.shape[2]*self.subsample, x.shape[3]*self.subsample)
-        out = T.nnet.abstract_conv.conv2d_grad_wrt_inputs(x, self.filters, output_shape, border_mode="half", subsample=(self.subsample, self.subsample))
-        out = out + self.bias.dimshuffle("x", 0, "x", "x")
-        return out
+        p0, p1 = self.padding[0], self.padding[1]
+        y = T.zeros((x.shape[0], x.shape[1], x.shape[2]+(2*p0), x.shape[3]+(2*p1)), dtype=floatX)
+        y = T.set_subtensor(y[:, :, p0:-p0, p1:-p1], x)
+        y = T.set_subtensor(y[:, :, :p0, p1:-p1], x[:, :, p0:0:-1, :])
+        y = T.set_subtensor(y[:, :, -p0:, p1:-p1], x[:, :, -2:-2-p0:-1])
+        y = T.set_subtensor(y[:, :, p0:-p0, :p1], x[:, :, :, p1:0:-1])
+        y = T.set_subtensor(y[:, :, p0:-p0, -p1:], x[:, :, :, -2:-2-p1:-1])
+        y = T.set_subtensor(y[:, :, :p0, :p1], x[:, :, p0:0:-1, p1:0:-1])
+        y = T.set_subtensor(y[:, :, -p0:, :p1], x[:, :, -2:-2-p0:-1, p1:0:-1])
+        y = T.set_subtensor(y[:, :, :p0, -p1:], x[:, :, p0:0:-1, -2:-2-p1:-1])
+        y = T.set_subtensor(y[:, :, -p0:, -p1:], x[:, :, -2:-2-p0:-1, -2:-2-p1:-1])
+        return y
 
     def get_output_shape_for(self, input_shape):
-        outh = None if input_shape[2] is None else input_shape[2]*self.subsample
-        outw = None if input_shape[3] is None else input_shape[3]*self.subsample
-        return (input_shape[0], self.nb_filter, outh, outw)
+        return (input_shape[0], input_shape[1], input_shape[2]+(2*self.padding[0]), input_shape[3]+(2*self.padding[1]))
 
 
-def conv_layer(in_, nb_filter, filter_length, subsample, only_conv=False):
-    out = Conv2D(nb_filter, filter_length, filter_length, subsample=(subsample, subsample), border_mode="same")(in_)
+def conv_layer(in_, nb_filter, filter_length, subsample=1, upsample=1, only_conv=False):
+    if upsample != 1:
+        out = UpSampling2D(size=(upsample, upsample))(in_)
+    else:
+        out = in_
+    padding = int(np.floor(filter_length / 2))
+    out = ReflectPadding2D((padding, padding))(out)
+    out = Conv2D(nb_filter, filter_length, filter_length, subsample=(subsample, subsample), border_mode="valid")(out)
     if not only_conv:
         out = InstanceNormalization()(out)
         out = Activation("relu")(out)
     return out
 
 
-def trans_conv_layer(in_, nb_filter, filter_length, subsample):
-    out = TransConv2D(nb_filter, filter_length, subsample)(in_)
-    out = InstanceNormalization()(out)
-    return Activation("relu")(out)
-
-
 def residual_block(in_):
-    out = conv_layer(in_, 128, 3, 1)
-    out = conv_layer(out, 128, 3, 1, True)
+    out = conv_layer(in_, 128, 3)
+    out = conv_layer(out, 128, 3, only_conv=True)
     return merge([out, in_], mode="sum")
 
 
 def get_transformer_net(X, weights=None):
-    input_ = Input(tensor=X, shape=(3, None, None))
-    y = conv_layer(input_, 32, 9, 1)
-    y = conv_layer(y, 64, 3, 2)
-    y = conv_layer(y, 128, 3, 2)
+    input_ = Input(tensor=X, shape=(3, 256, 256))
+    y = conv_layer(input_, 32, 9)
+    y = conv_layer(y, 64, 3, subsample=2)
+    y = conv_layer(y, 128, 3, subsample=2)
     y = residual_block(y)
     y = residual_block(y)
     y = residual_block(y)
     y = residual_block(y)
     y = residual_block(y)
-    y = trans_conv_layer(y, 64, 3, 2)
-    y = trans_conv_layer(y, 32, 3, 2)
-    y = conv_layer(y, 3, 9, 1, True)
+    y = conv_layer(y, 64, 3, upsample=2)
+    y = conv_layer(y, 32, 3, upsample=2)
+    y = conv_layer(y, 3, 9, only_conv=True)
     y = Activation("tanh")(y)
     y = Lambda(lambda x: (x * 150) + 127.5, output_shape=(3, None, None))(y)
     y = Lambda(lambda x: x - np.array([103.939, 116.779, 123.68], dtype=floatX).reshape(1, 3, 1, 1), output_shape=(3, None, None))(y)
