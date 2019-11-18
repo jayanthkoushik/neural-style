a33aff34b13b12ca30f1930150ded50036ffef23
Jayanth Koushik <jnkoushik@gmail.com>
2019-11-18 14:27:14 -0500

    Don't repeat existing custom js/css

--- .github/utils/setup.sh
+++ .github/utils/setup.sh
@@ -4,3 +4,6 @@ cp js/* /www/js/
 cp _includes/* /www/_includes/
 cp -r imgs /www/imgs/
 rm -r /www/fonts
+
+cat js/_custom.ext.js >> /www/js/_custom.js
+cat css/_custom.ext.scss >> /www/css/_custom.scss
